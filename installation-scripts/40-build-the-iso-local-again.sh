8a2b806fb880101d1ea15c304d1b61199ab09cf6
Erik Dubois <erik.dubois@gmail.com>
2023-03-23 14:22:55 +0100

    fixing

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -202,20 +202,20 @@ echo
 	echo
 	wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/etc/skel/.bashrc-latest -O $buildFolder/archiso/airootfs/etc/skel/.bashrc
 
-	echo "Removing the old pacman.conf file from build folder"
-	rm $buildFolder/archiso/pacman.conf
-	echo
-	echo "Copying the new pacman.conf file to the build folder"
-	cp -f ../archiso/pacman.conf $buildFolder/archiso/pacman.conf
-	echo
+	# echo "Removing the old pacman.conf file from build folder"
+	# rm $buildFolder/archiso/pacman.conf
+	# echo
+	# echo "Copying the new pacman.conf file to the build folder"
+	# cp -f ../archiso/pacman.conf $buildFolder/archiso/pacman.conf
+	# echo
 	
-	# echo "Removing the old packages.x86_64 file from build folder"
-	# rm $buildFolder/archiso/packages.x86_64
-	# rm $buildFolder/archiso/packages-personal-repo.x86_64
-	# echo
-	# echo "Copying the new packages.x86_64 file to the build folder"
-	# cp -f ../archiso/packages.x86_64 $buildFolder/archiso/packages.x86_64
-	# echo
+	echo "Removing the old packages.x86_64 file from build folder"
+	rm $buildFolder/archiso/packages.x86_64
+	rm $buildFolder/archiso/packages-personal-repo.x86_64
+	echo
+	echo "Copying the new packages.x86_64 file to the build folder"
+	cp -f ../archiso/packages.x86_64 $buildFolder/archiso/packages.x86_64
+	echo
 	
 	echo
 	if [ $personalrepo == true ]; then
